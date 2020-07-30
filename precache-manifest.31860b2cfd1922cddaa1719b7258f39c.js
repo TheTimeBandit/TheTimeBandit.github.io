f761edc2635bd444efb3ba3caa9dffeaffa58a1a
TheTimeBandit <tomixtt@gmail.com>
2020-07-30 22:55:55 +0200

    Updates

---
+++ precache-manifest.31860b2cfd1922cddaa1719b7258f39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e462a3ba09b782e0f91902a5c0207ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6105c4422305a65ca808",
+    "url": "/static/css/main.c4e1f26c.chunk.css"
+  },
+  {
+    "revision": "76032afa9c9e02a2e02d",
+    "url": "/static/js/2.37ce93d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37ce93d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6105c4422305a65ca808",
+    "url": "/static/js/main.2e004682.chunk.js"
+  },
+  {
+    "revision": "1d9cae230a989c08d3fa",
+    "url": "/static/js/runtime-main.fde97af0.js"
+  }
+]);
